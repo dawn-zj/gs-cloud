19f4248f6e8146bb32f3d292758760caa0b16052
dawn-zj <867096367@qq.com>
2021-11-23 18:45:50 +0800

    网关

---
+++ gs-gateway/src/main/java/com/gs/gsgateway/GsGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gs.gsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsGatewayApplication.class, args);
+    }
+
+}
