285aa0517ddad282a4f4673edc7de6cb54500d05
dawn-zj <867096367@qq.com>
2021-11-23 10:07:00 +0800

    项目打war包，用继承SpringBootServletInitializer重写configure方法的方式实现

--- gs-webserver/src/main/java/com/gs/gswebserver/GsWebserverApplication.java
+++ gs-webserver/src/main/java/com/gs/gswebserver/GsWebserverApplication.java
@@ -2,13 +2,19 @@ package com.gs.gswebserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GsWebserverApplication {
+public class GsWebserverApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GsWebserverApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GsWebserverApplication.class);
     }
 
 }
