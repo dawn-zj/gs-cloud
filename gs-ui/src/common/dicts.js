7b1f44997da15ee30f90633f089e762c886e32d1
dawn-zj <867096367@qq.com>
2021-11-23 18:26:10 +0800

    ui页面

---
+++ gs-ui/src/common/dicts.js
@@ -0,0 +1,22 @@
+export const dict = {
+  sys_user_sex: [
+    {"dictCode": "1", "dictValue": "男"},
+    {"dictCode": "2", "dictValue": "女"},
+    {"dictCode": "3", "dictValue": "未知"}
+  ],
+  sys_normal_disable: [
+    {"dictCode": "1", "dictValue": "正常"},
+    {"dictCode": "2", "dictValue": "停用"}
+  ]
+}
+
+
+export function getDictValue(dictType, dictCode) {
+  let dictValue = ''
+    dict[dictType].forEach(item => {
+    if (item.dictCode === dictCode){
+      dictValue = item.dictValue;
+    }
+  })
+  return dictValue
+}
