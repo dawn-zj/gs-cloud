c5b8458d5978eacc911eb4c536cdf6088e3fa476
dawn-zj <867096367@qq.com>
2022-03-03 11:38:18 +0800

    后台变量修改

--- gs-ui/src/components/Editor/index.vue
+++ gs-ui/src/components/Editor/index.vue
@@ -84,7 +84,7 @@ export default {
           }
         }
       },
-      uploadImgUrl: process.env.VUE_APP_BASE_API + '/common/upload', // 上传的图片服务器地址
+      uploadImgUrl: process.env.VUE_APP_WEB_API + '/common/upload', // 上传的图片服务器地址
       headers: {
         Authorization: 'Bearer ' + getToken()
       }
