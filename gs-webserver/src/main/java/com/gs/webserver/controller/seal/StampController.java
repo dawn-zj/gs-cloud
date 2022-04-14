81a4a79ce9282f35618e5eb6e5d1a7cd615f1146
dawn-zj <867096367@qq.com>
2022-04-14 11:45:41 +0800

    初步实现pdf验签名

---
+++ gs-webserver/src/main/java/com/gs/webserver/controller/seal/StampController.java
@@ -0,0 +1,25 @@
+package com.gs.webserver.controller.seal;
+
+import com.gs.webserver.entity.to.response.ResponseTo;
+import com.gs.webserver.service.IStampService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/seal/stamp")
+public class StampController {
+    @Autowired
+    @Qualifier("pdfStampServiceImpl")
+    private IStampService stampService;
+
+    @PostMapping("/verify")
+    public ResponseTo verify(@RequestParam("file") MultipartFile file) {
+        boolean verify = stampService.verify(file);
+        return ResponseTo.success(verify);
+    }
+}
