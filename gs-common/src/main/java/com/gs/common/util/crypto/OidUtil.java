c535746427c72145d5495895e07abdd41b908686
dawn-zj <867096367@qq.com>
2022-05-30 22:21:48 +0800

    OID

---
+++ gs-common/src/main/java/com/gs/common/util/crypto/OidUtil.java
@@ -0,0 +1,36 @@
+package com.gs.common.util.crypto;
+
+public class OidUtil {
+    public static final String OID_MD2 = "1.2.840.113549.2.2";
+
+    public static final String OID_MD5 = "1.2.840.113549.2.5";
+
+    public static final String OID_SHA1 = "1.3.14.3.2.26";
+
+    public static final String OID_SHA224 = "2.16.840.1.101.3.4.2.4";
+
+    public static final String OID_SHA256 = "2.16.840.1.101.3.4.2.1";
+
+    public static final String OID_SHA384 = "2.16.840.1.101.3.4.2.2";
+
+    public static final String OID_SHA512 = "2.16.840.1.101.3.4.2.3";
+
+    public static final String OID_SM3 = "1.2.156.10197.1.401";
+
+    public static final String OID_RSA = "1.2.840.113549.1.1.1";
+
+    public static final String OID_SM2 = "1.2.156.10197.1.301.1";
+
+    public static final String OID_SM1 = "1.2.156.10197.1.102";
+
+    public static final String OID_MD2withRSA = "1.2.840.113549.1.1.2";
+
+    public static final String OID_MD5withRSA = "1.2.840.113549.1.1.4";
+
+    public static final String OID_SHA1withRSA = "1.2.840.113549.1.1.5";
+
+    public static final String OID_SHA256withRSA = "1.2.840.113549.1.1.11";
+
+    public static final String OID_SM3withSM2 = "1.2.156.10197.1.501";
+
+}
