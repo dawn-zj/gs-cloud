4d6fe1af7665e60b5e675ecd0d4479ac1627c905
dawn-zj <867096367@qq.com>
2022-06-23 15:17:13 +0800

    增加磁盘信息

--- gs-webserver/src/main/java/com/gs/webserver/entity/to/request/ServerTo.java
+++ gs-webserver/src/main/java/com/gs/webserver/entity/to/request/ServerTo.java
@@ -1,14 +1,18 @@
 package com.gs.webserver.entity.to.request;
 
 import com.gs.common.entity.Cpu;
+import com.gs.common.entity.Disk;
 import com.gs.common.entity.Memory;
 import com.gs.common.entity.ServerInfo;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class ServerTo {
     private ServerInfo server;
     private Memory memory;
     private Cpu cpu;
+    private List<Disk> disks;
 
 }
--- gs-webserver/src/main/java/com/gs/webserver/service/impl/MonitorServiceImpl.java
+++ gs-webserver/src/main/java/com/gs/webserver/service/impl/MonitorServiceImpl.java
@@ -14,6 +14,7 @@ public class MonitorServiceImpl implements IMonitorService {
         serverTo.setServer(NetWorkUtil.systemInfo());
         serverTo.setMemory(NetWorkUtil.memoryInfo());
         serverTo.setCpu(NetWorkUtil.cpuInfo());
+        serverTo.setDisks(NetWorkUtil.diskInfo());
         return serverTo;
     }
 }
