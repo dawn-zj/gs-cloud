d9b39ff517a89457843aca7f776c12a01fd5ab49
dawn-zj <867096367@qq.com>
2023-06-30 17:22:45 +0800

    修改布局，支持下载成指定文件类型

--- gs-ui/src/views/system/tool/base64/textBase64.vue
+++ gs-ui/src/views/system/tool/base64/textBase64.vue
@@ -4,7 +4,7 @@
       <el-tab-pane label="编码" name="1">
         <el-form ref="formEncode" :model="formDataEncode" label-width="80px">
           <el-row>
-            <el-col :xs="24" :sm="24" :md="10" :lg="10">
+            <el-col>
               <el-form-item label="原文" required>
                 <el-input
                   v-model="formDataEncode.content"
@@ -12,12 +12,15 @@
                   :autosize="{ minRows: 5, maxRows: 10}"
                 />
               </el-form-item>
-
             </el-col>
-            <el-col :xs="24" :sm="24" :md="2" :lg="2" :offset="1">
-              <el-row><el-button type="primary" plain class="mt10" @click="handleEncode">编码</el-button></el-row>
+          </el-row>
+          <el-row>
+            <el-col :offset="10">
+              <el-button type="primary" plain class="mb20" @click="handleEncode">编码</el-button>
             </el-col>
-            <el-col :xs="24" :sm="24" :md="10" :lg="10">
+          </el-row>
+          <el-row>
+            <el-col>
               <el-form-item label="编码数据">
                 <el-input
                   v-model="formDataEncode.contentB64"
@@ -29,10 +32,11 @@
           </el-row>
         </el-form>
       </el-tab-pane>
+
       <el-tab-pane label="解码" name="2">
         <el-form ref="formDecode" :model="formDataDecode" label-width="80px">
           <el-row>
-            <el-col :xs="24" :sm="24" :md="10" :lg="10">
+            <el-col>
               <el-form-item label="编码数据" required>
                 <el-input
                   v-model="formDataDecode.contentB64"
@@ -41,11 +45,24 @@
                 />
               </el-form-item>
             </el-col>
-            <el-col :xs="24" :sm="24" :md="2" :lg="2" :offset="1">
-              <el-row><el-button type="success" plain class="mt10" @click="handleDecode">解码</el-button></el-row>
-              <el-row><el-button type="success" plain class="mt10" @click="handleDecodeAndDownload">解码后下载</el-button></el-row>
+          </el-row>
+          <el-row>
+            <el-col>
+              <el-form-item label="下载类型" required>
+                <el-radio v-model="fileType" label=".txt">txt</el-radio>
+                <el-radio v-model="fileType" label=".pdf">pdf</el-radio>
+                <el-radio v-model="fileType" label=".ofd">ofd</el-radio>
+              </el-form-item>
             </el-col>
-            <el-col :xs="24" :sm="24" :md="10" :lg="10">
+          </el-row>
+          <el-row>
+            <el-col :offset="10">
+              <el-button type="success" plain class="mb20" @click="handleDecode">解码</el-button>
+              <el-button type="success" plain class="mb20" @click="handleDecodeAndDownload">解码后下载</el-button>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col>
               <el-form-item label="原文">
                 <el-input
                   v-model="formDataDecode.content"
@@ -65,14 +82,16 @@
 
 import { downloadBase64 } from '@/utils/file'
 import { base64Decode, base64Encode } from '@/utils/base64/base64'
+import { parseTime } from '@/utils/ruoyi'
 
 export default {
   name: 'Index',
   data() {
     return {
-      tabPosition: 'top',
       // 用于各tab里的组件点击tab时渲染
+      tabPosition: 'top',
       activeName: '1',
+      fileType: '.txt',
       formDataEncode: {
         content: '',
         contentB64: ''
@@ -95,7 +114,9 @@ export default {
     // 解码并下载
     handleDecodeAndDownload() {
       this.formDataDecode.content = base64Decode(this.formDataDecode.contentB64)
-      downloadBase64(this.formDataDecode.contentB64, 'decode.txt')
+      const timestamp = new Date().getTime()
+      const localTime = parseTime(timestamp, '{y}{m}{d}{h}{i}{s}')
+      downloadBase64(this.formDataDecode.contentB64, 'decode-' + localTime + this.fileType)
     }
   }
 }
