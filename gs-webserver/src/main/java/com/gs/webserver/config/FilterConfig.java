a659b055b1666883dca61831535f94c1cd8646b0
dawn-zj <867096367@qq.com>
2023-12-12 09:44:58 +0800

    支持跨域

---
+++ gs-webserver/src/main/java/com/gs/webserver/config/FilterConfig.java
@@ -0,0 +1,37 @@
+package com.gs.webserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Filter配置
+ * @Author Zhang Juan
+ * @Date 2023/12/12 9:35
+ */
+@Configuration
+public class FilterConfig {
+
+    /**
+     * 跨域配置
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // 设置访问源地址
+        config.addAllowedOrigin("*");
+        // 设置访问源请求头
+        config.addAllowedHeader("*");
+        // 设置访问源请求方法
+        config.addAllowedMethod("*");
+        // 对接口配置跨域设置
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
+
