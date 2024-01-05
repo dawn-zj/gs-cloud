140123ed66e5ca1a9528f14e851e3d3593067a97
dawn-zj <867096367@qq.com>
2024-01-05 15:38:06 +0800

    规范注释

--- gs-webserver/src/main/java/com/gs/webserver/controller/tool/Base64Controller.java
+++ gs-webserver/src/main/java/com/gs/webserver/controller/tool/Base64Controller.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 /**
  * 编解码
+ * @author Administator
  */
 @RestController
 @RequestMapping("/tool/base64")
@@ -16,11 +17,11 @@ public class Base64Controller {
 
     /**
      * 编码
-     * @param base64To
-     * @return
+     * @param base64To 请求数据
+     * @return 编码结果
      */
     @PostMapping("/encode")
-    public ResponseTo<CommonResTo> encode(@RequestBody Base64To base64To) {
+    public ResponseTo<CommonResTo> encode(@RequestBody Base64To.Base64ReqTo base64To) {
         String encode = Base64Util.encode(base64To.getContent());
         CommonResTo commonTo = new CommonResTo();
         commonTo.setResult(encode);
@@ -29,12 +30,12 @@ public class Base64Controller {
 
     /**
      * 解码
-     * @param base64To
-     * @return
+     * @param base64To 请求数据
+     * @return 解码结果
      */
     @PostMapping("/decode")
-    public ResponseTo<CommonResTo> decode(@RequestBody Base64To base64To) {
-        byte[] decode = Base64Util.decode(base64To.getContentB64());
+    public ResponseTo<CommonResTo> decode(@RequestBody Base64To.Base64ReqTo base64To) {
+        byte[] decode = Base64Util.decode(base64To.getContent());
         CommonResTo commonTo = new CommonResTo();
         commonTo.setResult(StringUtil.getString(decode));
         return ResponseTo.success(commonTo);
