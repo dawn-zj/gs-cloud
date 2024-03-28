ef60fbdb4ce3343ac0c7750b7052e6cb179beca3
dawn-zj <867096367@qq.com>
2024-03-28 10:35:40 +0800

    支持去除指定RGB的背景色

--- gs-webserver/src/main/java/com/gs/webserver/controller/tool/PhotoController.java
+++ gs-webserver/src/main/java/com/gs/webserver/controller/tool/PhotoController.java
@@ -1,7 +1,5 @@
 package com.gs.webserver.controller.tool;
 
-import com.gs.common.define.Constants;
-import com.gs.common.util.FileUtil;
 import com.gs.common.util.ImageUtil;
 import com.gs.common.util.base64.Base64Util;
 import com.gs.webserver.entity.to.request.BaseTo;
@@ -9,10 +7,11 @@ import com.gs.webserver.entity.to.response.CommonResTo;
 import com.gs.webserver.entity.to.request.PhotoTo;
 import com.gs.webserver.entity.to.response.ResponseTo;
 import com.gs.webserver.service.IPhotoService;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * 图片处理
@@ -61,7 +60,8 @@ public class PhotoController {
      * @throws Exception
      */
     @GetMapping("/genCodeImg")
-    public ResponseTo<CommonResTo> genAuthCodeTest(int width, int height, int codeCount, int lineCount) throws Exception {
+    public ResponseTo<CommonResTo> genAuthCodeTest(@NotNull int width, @NotNull int height,
+                                                   @NotNull int codeCount, @NotNull int lineCount) throws Exception {
         byte[] photoData = ImageUtil.genAuthCodeImage(width, height, codeCount, lineCount);
         CommonResTo commonTo = new CommonResTo();
         commonTo.setResult(Base64Util.encode(photoData));
@@ -69,15 +69,19 @@ public class PhotoController {
     }
 
     /**
-     * 去白色背景
+     * 去背景
      * @param file 图片文件
+     * @param imgR 指定颜色的R值 |255
+     * @param imgG 指定颜色的G值 |255
+     * @param imgB 指定颜色的B值 |255
      * @return
      * @throws Exception
      */
     @PostMapping("/cleanBGColor")
-    public ResponseTo<CommonResTo> cleanBGColorTest(@RequestParam("file") MultipartFile file) throws Exception {
-        // cleanBGColor内部针对指定白色背景进行处理，适用于大部分图片
-        byte[] photoData = ImageUtil.cleanBGColor(file.getBytes());
+    public ResponseTo<CommonResTo> cleanBGColorTest(@RequestParam("file") MultipartFile file,
+                                                    @NotNull int imgR, @NotNull int imgG, @NotNull int imgB) throws Exception {
+        // cleanBGColor内部针对指定颜色背景进行处理，适用于大部分图片
+        byte[] photoData = ImageUtil.cleanBGColor(file.getBytes(), imgR, imgG, imgB);
         CommonResTo commonTo = new CommonResTo();
         commonTo.setResult(Base64Util.encode(photoData));
         return ResponseTo.success(commonTo);
