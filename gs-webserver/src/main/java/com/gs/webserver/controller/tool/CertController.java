afef6637be555e3322a22929529d5ed060484c2c
dawn-zj <867096367@qq.com>
2024-03-28 10:34:30 +0800

    生成和解析p10

--- gs-webserver/src/main/java/com/gs/webserver/controller/tool/CertController.java
+++ gs-webserver/src/main/java/com/gs/webserver/controller/tool/CertController.java
@@ -1,7 +1,11 @@
 package com.gs.webserver.controller.tool;
 
+import com.gs.common.entity.KeyStoreIo;
 import com.gs.common.util.HexUtil;
+import com.gs.common.util.base64.Base64Util;
 import com.gs.common.util.cert.CertUtil;
+import com.gs.common.util.p10.P10Util;
+import com.gs.webserver.entity.to.response.CommonResTo;
 import com.gs.webserver.entity.to.response.ResponseTo;
 import com.gs.webserver.entity.to.response.cert.CertResTo;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,9 +31,9 @@ public class CertController {
      * @throws Exception
      */
     @PostMapping("/getX509Cert")
-    public ResponseTo<CertResTo> certTest(@RequestParam("file") MultipartFile file) throws Exception {
+    public ResponseTo<CertResTo.CertTo> certTest(@RequestParam("file") MultipartFile file) throws Exception {
         X509Certificate x509Certificate = CertUtil.getX509Certificate(file.getBytes());
-        CertResTo certResTo = new CertResTo();
+        CertResTo.CertTo certResTo = new CertResTo.CertTo();
         certResTo.setDn(x509Certificate.getSubjectDN().toString());
         certResTo.setIssueDn(x509Certificate.getIssuerDN().toString());
         certResTo.setSn(HexUtil.byte2Hex(x509Certificate.getSerialNumber().toByteArray()).toUpperCase());
@@ -38,4 +42,43 @@ public class CertController {
         certResTo.setKeyUsage(x509Certificate.getKeyUsage());
         return ResponseTo.success(certResTo);
     }
+
+    /**
+     * 生成证书请求
+     * @param alg 签名算法 |RSA
+     * @param subjectDn 证书主题 |CN=Test
+     * @return
+     * @throws Exception
+     */
+    @PostMapping("/genP10")
+    public ResponseTo<CommonResTo> genP10(String alg, String subjectDn) throws  Exception {
+        KeyStoreIo keyStoreIo = P10Util.genP10(".pri", alg, subjectDn);
+        String p10 = keyStoreIo.getP10();
+
+        CommonResTo commonResTo = new CommonResTo();
+        commonResTo.setResult(p10);
+        return ResponseTo.success(commonResTo);
+    }
+
+    /**
+     * 解析证书请求
+     * @param p10B64 证书请求
+     * @return
+     * @throws Exception
+     */
+    @PostMapping("/verifyP10")
+    public ResponseTo<CertResTo.CertRequestTo> verifyP10(String p10B64) throws  Exception {
+        byte[] p10Data = Base64Util.decode(p10B64);
+        boolean verifyP10 = P10Util.verifyP10(p10Data);
+        byte[] pubKeyData = P10Util.getPubKeyFormP10(p10Data);
+
+        CertResTo.CertRequestTo certRequestTo = new CertResTo.CertRequestTo();
+        certRequestTo.setResult(verifyP10);
+        certRequestTo.setPublicKeyB64(Base64Util.encode(pubKeyData));
+
+        return ResponseTo.success(certRequestTo);
+    }
+
+
+
 }
--- gs-webserver/src/main/java/com/gs/webserver/entity/to/response/cert/CertResTo.java
+++ gs-webserver/src/main/java/com/gs/webserver/entity/to/response/cert/CertResTo.java
@@ -7,44 +7,61 @@ import lombok.Data;
  */
 @Data
 public class CertResTo {
-    /**
-     * 证书序列号
-     */
-    private String sn;
-    /**
-     * 证书主题
-     */
-    private String dn;
-    /**
-     * 证书通用名称
-     */
-    private String cn;
-    /**
-     * 证书颁发者主题
-     */
-    private String issueDn;
-    /**
-     * 证书颁发者通用名称
-     */
-    private String issueCn;
-    /**
-     * 密钥用法
-     */
-    private boolean[] keyUsage;
-    /**
-     * 证书起始日期
-     */
-    private Long notBefore;
-    /**
-     * 证书结束日期
-     */
-    private Long notAfter;
-    /**
-     * 证书授权密钥标识符
-     */
-    private String keyId;
-    /**
-     * 证书颁发者密钥标识
-     */
-    private String keyIdIssue;
+
+    @Data
+    public static class CertTo {
+        /**
+         * 证书序列号
+         */
+        private String sn;
+        /**
+         * 证书主题
+         */
+        private String dn;
+        /**
+         * 证书通用名称
+         */
+        private String cn;
+        /**
+         * 证书颁发者主题
+         */
+        private String issueDn;
+        /**
+         * 证书颁发者通用名称
+         */
+        private String issueCn;
+        /**
+         * 密钥用法
+         */
+        private boolean[] keyUsage;
+        /**
+         * 证书起始日期
+         */
+        private Long notBefore;
+        /**
+         * 证书结束日期
+         */
+        private Long notAfter;
+        /**
+         * 证书授权密钥标识符
+         */
+        private String keyId;
+        /**
+         * 证书颁发者密钥标识
+         */
+        private String keyIdIssue;
+    }
+
+    @Data
+    public static class CertRequestTo {
+        /**
+         * 验证结果
+         */
+        private boolean result;
+        /**
+         * 公钥
+         */
+        private String publicKeyB64;
+    }
+
 }
