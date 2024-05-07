81c8a8ce207f66917cd79e5c8df251e0b7c822d4
dawn-zj <867096367@qq.com>
2024-05-07 15:43:56 +0800

    优化，增加更多可修改项

--- gs-ui/src/views/tool/buildForm/rules/link.js
+++ gs-ui/src/views/tool/buildForm/rules/link.js
@@ -16,7 +16,10 @@ export default {
       children: ['这是一段文字'],
       props: {
         href: 'https://www.baidu.com/',
-        target: '_blank'
+        target: '_blank',
+        type: '',
+        disabled: false,
+        underline: false
       }
     };
   },
@@ -43,12 +46,35 @@ export default {
           type: 'textarea'
         }
       },
-      , {
+      {
+        type: 'select',
+        field: 'type',
+        title: '链接类型',
+        options: [
+          { label: '默认链接', value: '' },
+          { label: '主要链接', value: 'primary' },
+          { label: '成功链接', value: 'success' },
+          { label: '警告链接', value: 'warning' },
+          { label: '危险链接', value: 'danger' },
+          { label: '信息链接', value: 'info' }
+        ]
+      },
+      {
         type: 'select',
         field: 'target',
         title: '打开方式',
-        options: [{label: '在新窗口打开', value: '_blank'}, {label: '在当前窗口中打开', value: '_self'}]
+        options: [
+          {label: '在新窗口打开', value: '_blank'},
+          {label: '在当前窗口中打开', value: '_self'}
+        ]
       },
+      { type: 'switch', field: 'disabled', title: '是否禁用状态' },
+      { type: 'switch', field: 'underline', title: '是否有下划线' },
+      {
+        type: 'input',
+        field: 'icon',
+        title: '图标类名'
+      }
     ];
   }
 };
